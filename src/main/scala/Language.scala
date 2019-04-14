be0a08f43cccd6dbce879260c837042aad3a6538
Andrew Smith <smith.andrew.l@gmail.com>
2019-04-13 19:34:45 -0500

    Code cleanup.

--- src/main/scala/Language.scala
+++ src/main/scala/Language.scala
@@ -1,16 +1,16 @@
 object Language {
   sealed abstract class Expr
-  case class BooleanType(value: Boolean) extends Expr
-  case class AndOperation(left: BooleanType, right: BooleanType) extends Expr
-  case class OrOperation(left: BooleanType, right: BooleanType) extends Expr
+  case class BooleanType(value: Boolean)                           extends Expr
+  case class AndOperation(left: BooleanType,  right: BooleanType)  extends Expr
+  case class OrOperation(left:  BooleanType,  right: BooleanType)  extends Expr
 
   def evaluate(expr: Expr) = expr match {
     case BooleanType(x) => BooleanType(x)
-    case AndOperation(BooleanType(false), BooleanType(_)) => BooleanType(false)
-    case AndOperation(BooleanType(_), BooleanType(false)) => BooleanType(false)
-    case AndOperation(BooleanType(_), BooleanType(_)) => BooleanType(true)
-    case OrOperation(BooleanType(true), BooleanType(_)) => BooleanType(true)
-    case OrOperation(BooleanType(_), BooleanType(true)) => BooleanType(true)
-    case OrOperation(BooleanType(_), BooleanType(_)) => BooleanType(false)
+    case AndOperation(BooleanType(false), BooleanType(_))     => BooleanType(false)
+    case AndOperation(BooleanType(_),     BooleanType(false)) => BooleanType(false)
+    case AndOperation(BooleanType(_),     BooleanType(_))     => BooleanType(true)
+    case OrOperation(BooleanType(true),   BooleanType(_))     => BooleanType(true)
+    case OrOperation(BooleanType(_),      BooleanType(true))  => BooleanType(true)
+    case OrOperation(BooleanType(_),      BooleanType(_))     => BooleanType(false)
   }
 }
