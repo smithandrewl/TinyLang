174c060532c4ef3f9831489cb79e5ea15325b11e
Andrew Smith <smith.andrew.l@gmail.com>
2019-04-13 19:39:46 -0500

    Code cleanup.

--- src/test/scala/TinyLangTests.scala
+++ src/test/scala/TinyLangTests.scala
@@ -1,46 +1,110 @@
-
-
-
 import utest._
-
 import Language._
 
 package root {
 
-object TinyLangTests extends TestSuite {
-  val tests = Tests {
-    'Boolean - {
-       'TrueIsTrue - {
-         evaluate(BooleanType(true)) == BooleanType(true)
-       }
+  object TinyLangTests extends TestSuite {
+    val tests = Tests {
+      'Boolean - {
+        'TrueIsTrue - {
+          evaluate(BooleanType(true)) == BooleanType(true)
+        }
 
-      'FalseIsFalse - {
-        assert(evaluate(BooleanType(false)) == BooleanType(false))
+        'FalseIsFalse - {
+          assert(evaluate(BooleanType(false)) == BooleanType(false))
+        }
+      }
+      'And - {
+        'FalseAndBooleanIsFalse - {
+          assert(
+            evaluate(
+              AndOperation(
+                BooleanType(false),
+                BooleanType(false)
+              )
+            ) == BooleanType(false)
+          )
+
+          assert(
+            evaluate(
+              AndOperation(
+                BooleanType(false),
+                BooleanType(true)
+              )
+            ) == BooleanType(false)
+          )
+        }
+        'BooleanAndFalseIsFalse - {
+          assert(
+            evaluate(
+              AndOperation(
+                BooleanType(false),
+                BooleanType(false)
+              )
+            ) == BooleanType(false)
+          )
+
+          assert(
+            evaluate(
+              AndOperation(
+                BooleanType(true),
+                BooleanType(false)
+              )
+            ) == BooleanType(false)
+          )
+        }
+        'TrueAndTrueIsTrue - {
+          assert(
+            evaluate(
+              AndOperation(
+                BooleanType(true),
+                BooleanType(true)
+              )
+            ) == BooleanType(true)
+          )
+        }
+      }
+
+      'Or - {
+        'TrueOrBooleanIsTrue - {
+          assert(
+            evaluate(
+              OrOperation(
+                BooleanType(true),
+                BooleanType(false)
+              )
+            ) == BooleanType(true)
+          )
+
+          assert(
+            evaluate(
+              OrOperation(
+                BooleanType(false),
+                BooleanType(true)
+              )
+            ) == BooleanType(true)
+          )
+
+          assert(
+            evaluate(
+              OrOperation(
+                BooleanType(true),
+                BooleanType(true)
+              )
+            ) == BooleanType(true)
+          )
+
+          assert(
+            evaluate(
+              OrOperation(
+                BooleanType(false),
+                BooleanType(false)
+              )
+            ) == BooleanType(false)
+          )
+        }
       }
     }
-    'And - {
-      'FalseAndBooleanIsFalse - {
-        assert(evaluate(AndOperation(BooleanType(false), BooleanType(false))) == BooleanType(false))
-        assert(evaluate(AndOperation(BooleanType(false), BooleanType(true))) == BooleanType(false))
-      }
-      'BooleanAndFalseIsFalse - {
-        assert(evaluate(AndOperation(BooleanType(false), BooleanType(false))) == BooleanType(false))
-        assert(evaluate(AndOperation(BooleanType(true), BooleanType(false))) == BooleanType(false))
-      }
-      'TrueAndTrueIsTrue - {
-        assert(evaluate(AndOperation(BooleanType(true), BooleanType(true))) == BooleanType(true))
-      }
-    }
-
-    'Or - {
-      'TrueOrBooleanIsTrue - {
-        assert(evaluate(OrOperation(BooleanType(true), BooleanType(false))) == BooleanType(true))
-        assert(evaluate(OrOperation(BooleanType(false), BooleanType(true))) == BooleanType(true))
-        assert(evaluate(OrOperation(BooleanType(true), BooleanType(true))) == BooleanType(true))
-        assert(evaluate(OrOperation(BooleanType(false), BooleanType(false))) == BooleanType(false))
-      }
-    }
-
   }
-}
+
 }
