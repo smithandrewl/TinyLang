3e8720d3e36283c0e7e948ac566e9a9f6bb11665
Andrew Smith <smith.andrew.l@gmail.com>
2019-04-20 17:32:17 -0500

    Actually removed the old list tests since they have been moved.

--- src/test/scala/TinyLangTests.scala
+++ src/test/scala/TinyLangTests.scala
@@ -386,34 +386,6 @@ package root {
             )
           }
         }
-
-        'ListTests {
-          'AppendTests {
-            'AppendOneToEmptyList {
-              assert(
-                evaluate(
-                  AppendOperation(IntegerType(1), EmptyList())
-                ) == ListType(IntegerType(1), EmptyList())
-              )
-            }
-            'AppendTwoToListOfOne {
-              assert(
-                evaluate(
-                  AppendOperation(IntegerType(2), ListType(IntegerType(1), EmptyList()))
-                ) == ListType(IntegerType(2), ListType(IntegerType(1), EmptyList()))
-              )
-            }
-
-            'AppendTrueToListOfOne {
-              assert(
-                evaluate(
-                  AppendOperation(BooleanType(true), ListType(IntegerType(1), EmptyList()))
-                ) == ListType(BooleanType(true), ListType(IntegerType(1), EmptyList()))
-              )
-            }
-          }
-        }
-
       }
     }
   }
